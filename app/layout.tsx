e95c613a99d72fe839e491b63895d0d9a9461b70
emilitogaza <144819695+emilitogaza@users.noreply.github.com>
2026-01-29 18:56:44 +0100

    biome

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,20 @@
 import type { Metadata } from "next";
-import { Bricolage_Grotesque } from "next/font/google";
+import { Bricolage_Grotesque, Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+const geistSans = Geist({
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
+});
+
 const bricolageGrotesque = Bricolage_Grotesque({
-  variable: "--font-bricolage-grotesque",
-  subsets: ["latin"],
+	variable: "--font-bricolage-grotesque",
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -19,9 +29,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${bricolageGrotesque.variable} antialiased`}>
-        {children}
-      </body>
+      <body
+				className={`${geistSans.variable} ${geistMono.variable} ${bricolageGrotesque.variable} antialiased`}
+			>
+				{children}
+			</body>
     </html>
   );
 }
